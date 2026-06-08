d33e8b6b87bba2a409337d523a37fbaeb51dedd7
sir micah <paulmicah77@gmail.com>
2026-06-08 12:12:13 +0100

    Update site title and favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,15 @@
 export const metadata = {
-    title: "Identro",
+  title: "Identro",
   description: "Fast, secure and reliable identity services.",
   icons: {
     icon: "/favicon.png",
   },
 };
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
